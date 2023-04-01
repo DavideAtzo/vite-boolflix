5fa5a5842ca7788e7a59d842c6ad1a43aaba3736
DavideAtzo <atzo9477@gmail.com>
2023-04-01 15:56:54 +0200

    cambiato numero show title

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,7 +10,7 @@ export default {
     data() {
         return {
             store,
-            showTitle: 20
+            showTitle: 1
         }
     }
 
@@ -21,34 +21,36 @@ export default {
     <main>
         <section id="section-movies" class="container">
             <div id="films" v-show="store.films.length === showTitle">
-                <h1 class="mt-5 mx-4">Film trovati:</h1>
+                <h1 class="pt-5 mx-4">Film trovati:</h1>
             </div>
             <div class="row">
                 <AppFilmList />
             </div>
         </section>
         <section id="section-series" class="container mb-3">
-            <div id="films" v-show="store.series.length === showTitle">
-                <h1 class="mt-5 mx-4">Film trovati:</h1>
+            <div id="serie" v-show="store.series.length === showTitle">
+                <h1 class="mt-5 mx-4">Serie trovati:</h1>
             </div>
             <div class="row">
                 <AppSeriesList />
             </div>
-
         </section>
     </main>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/_partial/variables' as *;
-main{
+
+main {
     padding-bottom: 6.25rem;
-    h1{
+    background-color: $primary;
+
+    h1 {
         text-transform: uppercase;
         text-decoration: underline;
         font-weight: 700;
         color: $secondary;
-        
+
     }
 }
 </style>
